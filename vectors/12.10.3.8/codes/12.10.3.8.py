449192ed68d6851bdb58e054412a449413ab1512
Shantipriya1919 <107026608+Shantipriya1919@users.noreply.github.com>
2023-02-02 10:08:46 +0530

    Add files via upload

---
+++ vectors/12.10.3.8/codes/12.10.3.8.py
@@ -0,0 +1,11 @@
+from math import cos 
+import numpy as np
+import math
+import sys
+x=1/2 			#a.b
+y=np.cos(np.pi/3) 	#cos(60)
+z=x/y			#|a|^2=a.b/cos(60)
+a=np.sqrt(z)		#|a|
+b=a			#|a|=|b|
+print(round(a))
+print(round(b))
