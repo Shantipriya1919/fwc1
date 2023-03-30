78c1b6149f2d3ac314bca02932a7f49be25d2294
Shantipriya1919 <107026608+Shantipriya1919@users.noreply.github.com>
2023-03-30 10:34:26 +0530

    Add files via upload

---
+++ fpga/flash_m4.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+sudo python3 /home/shantipriya/TinyFPGA-Programmer-Application/tinyfpga-programmer-gui.py --port /dev/ttyACM0  --appfpga /home/shantipriya/fwc-1/fpga/helloworldfpga.bin --mode fpga 
+
